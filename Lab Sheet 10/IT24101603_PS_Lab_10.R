52fc05bd148a96206990da11131ecdf0d25f4320
IT24101603 <it24101603@my.sliit.lk>
2025-10-22 23:36:51 +0530

    IT24101603_PS_Lab_10

---
+++ Lab Sheet 10/IT24101603_PS_Lab_10.R
@@ -0,0 +1,35 @@
+#Q1
+
+## Define Hypotheses
+
+# H0: Customers choose all four snack types equally.
+# H1: Customers do not choose all four snack types equally.
+
+## Perform Chi-Square Test
+
+setwd("C:/Users/teran/OneDrive/Desktop/IT24101603")
+
+# Observed counts of purchases for each snack type
+observed <- c(120, 95, 85, 100)
+
+# Probabilities for each snack type (equal = 0.25)
+prob <- c(0.25, 0.25, 0.25, 0.25)
+
+# Chi-square goodness-of-fit test
+chi_result <- chisq.test(x = observed, p = prob)
+
+# Print result and observed vs expected
+print(chi_result)
+expected_counts <- sum(observed) * prob
+print(data.frame(Snack = c("A","B","C","D"),
+                 Observed = observed,
+                 Expected = expected_counts))
+
+## Conclusion
+
+#Significance level: α = 0.05
+#p-value = 0.089
+#Since p-value (0.089) > 0.05,
+#fail to reject H₀
+
+
